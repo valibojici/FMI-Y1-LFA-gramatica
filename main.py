14c39780c1c7c042c42fcf52f1092853c459f57f
Vali <valibojici@gmail.com>
2021-04-24 21:48:04 +0300

    added comments

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import wordGenerator as  wg
 import sys
 
-options = list(sys.argv)[1:]
+options = list(sys.argv)[1:]            # iau optiuni din cmd line daca exista
 wordGen = wg.WordGenerator()
 file_path = ''
 
-for i in range(0, len(options) - 1):
+for i in range(0, len(options) - 1):    # setez optiunile din cmd line
     if options[i] == '-t' and (options[i+1] == 'left' or options[i+1] == 'right'):
-        wordGen.setType(option[i+1])
+        wordGen.setType(options[i+1])
     elif options[i] == '-f':
         file_path = options[i+1]
 
 
-wordGen.setNonterminals(input('nonterminale : ').split())
-wordGen.setTerminals(input('terminale : ').split())
-wordGen.setStartSymbol(input('start : '))
+wordGen.setNonterminals(input('Neterminale (litere mari separate prin spatiu): ').split())
+wordGen.setTerminals(input('Terminale (litere mici separate prin spatiu): ').split())
+wordGen.setStartSymbol(input('Simbol start: '))
 
-nrProd = int(input('nrprod = '))
+nrProd = int(input('Numar productii: '))
 for i in range(nrProd):
-    prod = input('productie : ')
-    prod = prod.split(' ')
+    prod = input('Produtie ( <simbol> = <string>[|<string>...] $ = lambda) ')
     
-    wordGen.addProduction(prod[0], prod[1].strip().split('|'))
+    simbol = prod.split('=')[0].strip()                                         
+    strings = [string.strip() for string in prod.split('=')[1].strip().split('|')]
+    
+    wordGen.addProduction(simbol, strings)
 
 
-n = int(input('n = '))
+n = int(input('Lungime maxima: '))
 
 if file_path != '':
     wordGen.printWords(n, file_path)
--- wordGenerator.py
+++ wordGenerator.py
@@ -2,8 +2,7 @@ from collections import defaultdict
 
 class WordGenerator:
     def __init__(self, type = 'right'):
-        self.productions = defaultdict(set)
-        # dictionar -> cheie = neterminal; valoare = set de stringuri (terminale + 1 neterminal optional)
+        self.productions = defaultdict(set)     # dictionar -> cheie = neterminal; valoare = set de stringuri (terminale + 1 neterminal optional)
         self.nonterminals = set()
         self.terminals = set()
         self.startSymbol = ''
@@ -25,53 +24,58 @@ class WordGenerator:
         if symbol not in self.nonterminals:
             self.nonterminals.add(symbol)
         
-        for string in strings:
-            for char in string:
-                if char != '$' and (char not in self.nonterminals and char not in self.terminals):
-                    raise invalidLetter(char)
-
+        for string in strings:                            
+            for index, letter in enumerate(string):     # testez daca literele sunt neterminale sau terminale si sunt puse unde trebuie
+                if letter == '$':
+                    continue
+                if letter not in self.nonterminals and letter not in self.terminals:
+                    raise badProduction(f'{letter} not a terminal or nonterminal')
+                elif letter in self.nonterminals:
+                    if self.type == 'left' and index != 0:
+                        raise badProduction(f'{letter} not at start')
+                    elif self.type == 'right' and index != len(string) - 1:
+                        raise badProduction(f'{letter} not at end')
+        
         self.productions[symbol].update(strings)
 
 
     def wordGen(self, n):
-        words = defaultdict(list)
+        words = defaultdict(set)                # dictionar in care pun cuvintele generate
 
-        result = ['' for _ in range(n+1)]
+        result = ['' for _ in range(n+1)]       # 'stiva' in care generez cu backtracking partile din cuvinte
         
         def bkt(step, curr_len, symbol):
             for string in self.productions[symbol]:
-                nonterminal = string[-1] if self.type == 'right' else string[0]
+                nonterminal = string[-1] if self.type == 'right' else string[0]     # neterminalul este ori la inceput ori la final
 
-                if nonterminal == '$' or nonterminal not in self.nonterminals:
-                    result[step] = '' if nonterminal == '$' else string
+                if nonterminal == '$' or nonterminal not in self.nonterminals:      # daca nu exista neterminal (ultima sau prima litera e lambda sau terminal)
+                    result[step] = '' if nonterminal == '$' else string             # daca e lambda pun '' in loc de $
                     
-                    if len(result[step]) + curr_len <= n:
-                        if step == 0 and result[0] == '':
-                            words[0].append('$')
-                        else:
-                            word = ''.join(result[:step+1])
-                            words[len(word)].append(word)
-                else:
-                    result[step] = string[:-1] if self.type == 'right' else string[1:]
+                    if len(result[step]) + curr_len <= n:                           # daca lungimea cuv format pana acum e <= ca n atunci afisez
+                        word = ''.join(result[:step+1])
+                        words[len(word)].add(word)
+                else:                                                                   # daca exista neterminal la inceput sau sfarsit
+                    result[step] = string[:-1] if self.type == 'right' else string[1:]  # iau stringul fara neterminal
 
-                    if len(result[step]) + curr_len <= n:
+                    if len(result[step]) + curr_len <= n:                               # daca lungimea curenta e inca <= ca n atunci continui cu backtrackingul
                         bkt(step+1, len(result[step]) + curr_len, nonterminal)
 
         bkt(0,0,self.startSymbol)
         return words
 
     def printWords(self, max_length, file_path = None):
+        # functie care printeaza toate cuvintele de maxim max_length litere pe console sau in fisier
         wordDict = self.wordGen(max_length)
         
-        if file_path is not None:
+        if file_path is not None:                               # daca file_path exista atunci afisez in fisier
             with open(file_path, 'w') as g:
                 for len, words in sorted(wordDict.items()):
-                    g.write(f'{len} ->' + str(words) + '\n')
-        else:
+                    g.write(f'{len} -> ' + str(words) + '\n')
+        else:                                                   # altfel afisez pe consola
             for len, words in sorted(wordDict.items()):
                     print(f'{len} ->',words)
 
 
-class invalidLetter(Exception):
-    def __init__(self, letter):
-        super().__init__(f'{letter} is not terminal or nonterminal')
+class badProduction(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
